3fab1316dad6b699caab87f3d6587c58c8e9507f
Graeme Paterson <ghpaterson@gmail.com>
2023-10-17 11:49:37 +0100

    initial styling and page layout for homepage

--- app/page.js
+++ app/page.js
@@ -5,9 +5,29 @@ import NavBar from "@/components/NavBar";
 export default function Home() {
   return (
     <>
-      <main className="h-screen bg-blue-100 px-32">
+      <main className="h-screen bg-yellow-200 px-32">
         <NavBar />
-        <Link href="/MainMenu">Get Started</Link>
+        <section className="mt-24">
+          <p className="text-5xl">
+            Transform reading struggles into
+            <br />
+            reading<span className="font-bold text-blue-500">
+              {" "}
+              STRENGTHS
+            </span>{" "}
+            with our simple
+            <br />
+            exercises
+          </p>
+
+          <div className="my-12">
+            <Link href="/MainMenu">
+              <button className="text-xl border-blue-500 border-solid border-2 rounded-full px-2 py-2 hover:bg-blue-500 hover:text-white">
+                Get Started
+              </button>
+            </Link>
+          </div>
+        </section>
       </main>
     </>
   );
