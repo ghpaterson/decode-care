5810f4475f7aa3142a06e66904abf778a004d004
Graeme Paterson <ghpaterson@gmail.com>
2023-10-19 09:44:51 +0100

    used props to alter the values of each individual MainMenuCard

--- app/MainMenu/page.js
+++ app/MainMenu/page.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import NavBar from "@/components/NavBar";
 import Image from "next/image";
 import girlReading from "/public/girlReading.svg";
+import boyReading from "/public/boyReading.svg";
 import MainMenuCard from "@/components/MainMenuCard";
 
 export default function MainMenu() {
@@ -10,28 +11,22 @@ export default function MainMenu() {
       <main className="px-10 lg:px-32">
         <NavBar />
         <div className="flex flex-col gap-10 lg:py-16 font-poppins">
-          <MainMenuCard />
-          <MainMenuCard />
-          {/* <section class="hfw-action-card">
-            <div className="shadow-md min-w-full py-6">
-              <div className="flex flex-col lg:flex-row">
-                <div className="h-60 w-60 bg-gradient-to-b from-white to bg-yellow-500 flex justify-center">
-                  <Image src={girlReading} width={200} alt="boy reading" />
-                </div>
-                <div className="flex flex-col gap-4 px-6">
-                  <h1 className="lg:text-4xl">High Frequency Words</h1>
-                  <p>This is an example explaining the exercise</p>
-                </div>
-                <div className="flex lg:justify-end lg:items-end  lg:w-96 lg:ml-16">
-                  <Link href="/HFWMenu">
-                    <button className="bg-yellow-500 rounded-full text-xl lg:text-2xl text-white px-4 py-2">
-                      Play
-                    </button>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </section> */}
+          <MainMenuCard
+            imageScr={boyReading}
+            title="Real and Nonsense Words"
+            description="This is an example explaining the exercise"
+            linkHref="/CVCMenu"
+            cardColor="bg-gradient-to-b from-white to bg-blue-500"
+            imageWidth={150}
+          />
+          <MainMenuCard
+            imageScr={girlReading}
+            title="High Frequency Words"
+            description="This is an example explaining the exercise"
+            linkHref="/HFWMenu"
+            cardColor="bg-gradient-to-b from-white to bg-yellow-500"
+            imageWidth={200}
+          />
         </div>
       </main>
     </>
--- components/MainMenuCard.js
+++ components/MainMenuCard.js
@@ -2,21 +2,28 @@ import Link from "next/link";
 import Image from "next/image";
 import boyReading from "../public/boyReading.svg";
 
-export default function MainMenuCard() {
+export default function MainMenuCard({
+  imageScr,
+  title,
+  description,
+  linkHref,
+  cardColor,
+  imageWidth,
+}) {
   return (
     <main>
       <section class="cvc-action-card">
         <div className="shadow-md min-w-full py-6">
           <div className="flex flex-col lg:flex-row items-center lg:items-start">
-            <div className="h-60 w-60 bg-gradient-to-b from-white to bg-blue-500 flex justify-center">
-              <Image src={boyReading} width={150} alt="boy reading" />
+            <div className={`h-60 w-60 ${cardColor} flex justify-center`}>
+              <Image src={imageScr} width={imageWidth} alt="boy reading" />
             </div>
             <div className="flex flex-col gap-4 px-6 items-center lg:items-start">
-              <h1 className="lg:text-4xl">Real and Nonsense Words</h1>
-              <p>This is an example explaining the exercise</p>
+              <h1 className="lg:text-4xl">{title}</h1>
+              <p>{description}</p>
             </div>
             <div className="flex justify-end items-end lg:h-60 lg:w-96">
-              <Link href="/CVCMenu">
+              <Link href={linkHref}>
                 <button className="bg-blue-500 rounded-full text-xl lg:text-2xl text-white px-4 py-2">
                   Play
                 </button>
